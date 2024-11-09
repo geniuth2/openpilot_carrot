cdfcb0af25b02245ba5d8a9f93337d76755d6281
ajouatom <ajouatom@gmail.com>
2024-11-09 11:39:47 +0900

    ff

--- selfdrive/carrot/carrot_man.py
+++ selfdrive/carrot/carrot_man.py
@@ -1078,10 +1078,11 @@ class CarrotServ:
     CC = sm['carControl']
     if len(CC.orientationNED) == 3:
       bearing = math.degrees(CC.orientationNED[2])
-      print("CC.orientationNED[2] = {:.1f}, bearing={:.1f}".format(CC.orientationNED[2]), bearing)
+      print("CC.orientationNED[2] = {:.1f}, bearing={:.1f}".format(CC.orientationNED[2], bearing))
       location_valid = True
       self.bearing_offset = 0.0
     else:
+      print("CC.orientationNED[2] is not valid = {}".format(CC.orientationNED))
       location_valid = False
       bearing = 0.0
       return self.nPosAngle
