d1db33af20cb578c7723bb2577738a794a9acf56
geniuth <geniuth@gmail.com>
2024-11-24 22:23:36 +0900

    리젠 패들 테스트용 커밋

--- panda/board/safety/safety_gm.h
+++ panda/board/safety/safety_gm.h
@@ -105,7 +105,11 @@ static void gm_rx_hook(const CANPacket_t *to_push) {
     }
 
     if (addr == 0xBD) {
-      regen_braking = (GET_BYTE(to_push, 0) >> 4) != 0U;
+      regen_braking = (GET_BYTE(to_push, 0) >> 4) != 0U; // 1. 리젠 브레이크 상태 확인
+      // 2. 롱컨트롤 허용 여부 확인
+      if (!get_longitudinal_allowed()) {
+        //tx = false;
+      }
     }
 
     // Pedal Interceptor
@@ -244,7 +248,7 @@ static safety_config gm_init(uint16_t param) {
     .max_brake = 400,
   };
 
-  static const CanMsg GM_ASCM_TX_MSGS[] = {{0x180, 0, 4}, {0x409, 0, 7}, {0x40A, 0, 7}, {0x2CB, 0, 8}, {0x370, 0, 6}, {0x200, 0, 6}, {0x1E1, 0, 7},  // pt bus
+  static const CanMsg GM_ASCM_TX_MSGS[] = {{0x180, 0, 4}, {0x409, 0, 7}, {0x40A, 0, 7}, {0x2CB, 0, 8}, {0x370, 0, 6}, {0x200, 0, 6}, {0x1E1, 0, 7}, {0xBD, 0, 7},// pt bus
                                            {0xA1, 1, 7}, {0x306, 1, 8}, {0x308, 1, 7}, {0x310, 1, 2},   // obs bus
                                            {0x315, 2, 5}};  // ch bus
 
